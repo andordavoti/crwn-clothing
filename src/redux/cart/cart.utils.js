d9fbfe670bd7f819a50df96d073a563993be8123
Andor Davoti <andor.davoti@gmail.com>
2020-03-24 15:50:26 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+export const addItemToCart = (cartItems, cartItemToadd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToadd.id
+    )
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToadd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        )
+    }
+    else {
+        return [...cartItems, { ...cartItemToadd, quantity: 1 }]
+    }
+}
