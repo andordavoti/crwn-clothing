ade2f128e7a68fa804bc718a9b264a8931042af7
Andor Davoti <andor.davoti@gmail.com>
2020-04-05 15:52:40 +0200

    added checkout page to react router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
@@ -45,6 +47,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
         <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUp />} />
       </Switch>
     </div>
