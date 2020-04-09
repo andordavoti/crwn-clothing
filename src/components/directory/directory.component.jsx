afecd235dc010bb03291bec81f2f704814737c5e
Andor Davoti <andor.davoti@gmail.com>
2020-04-09 13:26:05 +0200

    moved shop collections to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,16 +6,15 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { selectDirectorySections } from '../../redux/directory/directory.selectors'
 
-const Directory = ({ sections }) => {
-
-    return <div className='directory-menu'>
+const Directory = ({ sections }) => (
+    <div className='directory-menu'>
         {
             sections.map(({ id, ...otherSectionProps }) => (
                 <MenuItem key={id} {...otherSectionProps} />
             ))
         }
     </div>
-}
+)
 
 const mapStateToProps = createStructuredSelector({
     sections: selectDirectorySections
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import SHOP_DATA from './shop.data'
+import { selectCollections } from '../../redux/shop/shop.selectors'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
-export default class ShopPage extends React.Component {
+const ShopPage = ({ collections }) => (
+    <div className='shop-page'>
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        }
+    </div>
+)
 
-    state = { collections: SHOP_DATA }
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-    render() {
-        const { collections } = this.state
-
-        return <div className='shop-page'>
-            {
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))
-            }
-        </div>
-    }
-}
+export default connect(mapStateToProps)(ShopPage)
