f34ae21f1aa1df0c03753085ad49e3b10bb4cfa0
Andor Davoti <andor.davoti@gmail.com>
2020-04-09 19:19:13 +0200

    added stripe to project, made stripe button component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeButton = ({ price }) => {
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_F5TFdEKeaDnFkSpCB1DIl40b00TYdprZ9q'
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return <StripeCheckout
+        label='Pay Now'
+        name='CRWN Clothing Ltd.'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/Cuz.svg'
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey} />
+}
+
+export default StripeButton
