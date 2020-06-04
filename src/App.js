51d1cd0a4168e3cb6aad0cd0724bf288c5b54ad9
Andor Davoti <andor.davoti@gmail.com>
2020-06-04 19:52:58 +0200

    moved shopdata to firebase

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ const App = () => {
                         })
                     );
                 });
-            } else dispatch(setCurrentUser(userAuth));
+            }
+            dispatch(setCurrentUser(userAuth));
         });
 
         return () => unsubscribeFromAuth();
